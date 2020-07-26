921cb6a4d6aec91405f23342fa9dcb3b78967201
Karar Al-Remahy <alremahykarar@ymail.com>
2020-07-26 19:12:39 +0200

    feat(Profile): Add /paginate end-point. Id for `/profile/:id` is no longer defined as optional.
    fix(template): profile.edge now uses `emote.url`.
    fix(template): updateProfile.edge now uses `item.url`.
    fix(template): commands.edge, clarified bot commands.

--- web/start/routes/profile.ts
+++ web/start/routes/profile.ts
@@ -2,7 +2,8 @@ import Route from '@ioc:Adonis/Core/Route'
 
 // RUD
 Route.get('/profile/:id?', 'ProfilesController.read').middleware('auth:web')
-Route.post('/profile/:id?', 'ProfilesController.update').middleware('auth:web')
+Route.post('/profile/:id', 'ProfilesController.update').middleware('auth:web')
 Route.post('/profile/:id/delete', 'ProfilesController.delete').middleware('auth:web')
 
 Route.post('/profile/:id/unmatch', 'ProfilesController.unmatch').middleware('auth:web')
+Route.post('/profile/:id/paginate', 'ProfilesController.matchesJSONPagination').middleware('auth:web')
