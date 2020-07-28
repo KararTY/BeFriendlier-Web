62561a321b615026b92c24b9cfe6bd2cb3bdf392
Karar Al-Remahy <alremahykarar@ymail.com>
2020-07-28 04:02:40 +0200

    feat: Now disconnects websockets without userAgent

--- start/socket.ts
+++ start/socket.ts
@@ -1,9 +1,24 @@
 import Ws from 'App/Services/Ws'
 
 Ws.start((socket, request) => {
-  console.log('New connection from:', request.socket.remoteAddress)
+  const userAgent = request.headers['user-agent']
+
+  if (userAgent === undefined) {
+    console.log(
+      'Terminating attempted websocket connection from (%s) %s:%n',
+      request.socket.remoteAddress,
+      request.socket.remotePort,
+      request.socket.remoteFamily,
+    )
+
+    socket.terminate()
+    return
+  }
+
+  console.log('New websocket connection by %s', userAgent)
 
   // On a new connection.
+  socket.id = userAgent
   socket.isAlive = true
   socket.connection = request.socket
 
