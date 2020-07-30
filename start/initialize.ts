4be99ba54d02c3dad44975841ccb76119e96e252
Karar Al-Remahy <alremahykarar@ymail.com>
2020-07-30 03:13:33 +0200

    fix:  Fix initialize not existing, it exists now.

---
+++ start/initialize.ts
@@ -0,0 +1,6 @@
+// import Ws from 'App/Services/Ws'
+
+export async function joinTwitchChannels () {
+  // TODO: Tell bots to join Twitch channels.
+  console.warn('This is not implemented yet!')
+}
