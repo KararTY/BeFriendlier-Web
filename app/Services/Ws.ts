393e92fb647d1404b735e980554190c05b5d206c
Karar Al-Remahy <alremahykarar@ymail.com>
2020-08-12 01:27:26 +0200

    feat: Escape potential pinged users in ROLLMATCH

--- app/Services/Ws.ts
+++ app/Services/Ws.ts
@@ -137,23 +137,40 @@ class Ws {
               const rm: ROLLMATCH = JSON.parse(res.data)
               const { user, profile } = await Handler.rollMatch(rm)
 
+              const bio = profile.bio.split(' ').map(word => `${word.substr(0, 1)}\u{E0000}${word.substr(1)}`).join(' ')
+
               switch (rm.more) {
                 case More.NONE:
-                  rm.result = { value: `new ${rm.global === true ? 'global ' : ''}match's bio: ${profile.bio.length > 64 ? `${String(profile.bio.substr(0, 32))}...` : String(profile.bio)}, reply with %prefix%more, %prefix%match or %prefix%no` }
+                  rm.result = {
+                    value: `new ${rm.global === true ? 'global ' : ''}match's bio: ` +
+                    `${profile.bio.length > 32 ? `${bio.substr(0, 32)}...` : bio}, reply with %prefix%more, %prefix%match or %prefix%no`,
+                  }
                   socket.send(this.socketMessage(MessageType.ROLLMATCH, JSON.stringify(rm)))
                   break
+
                 case More.BIO:
-                  rm.result = { value: `${rm.global === true ? 'global\'s ' : ''}full bio: ${String(profile.bio)}` }
+                  rm.result = {
+                    value: `${rm.global === true ? 'global\'s ' : ''}full bio: ` +
+                    `${bio}`,
+                  }
                   socket.send(this.socketMessage(MessageType.ROLLMATCH, JSON.stringify(rm)))
                   break
                 case More.FAVORITEEMOTES:
-                  rm.result = { value: `${rm.global === true ? 'global ' : ''}match's favorite emotes: ${profile.favoriteEmotes.length > 0 ? String(profile.favoriteEmotes.map(emote => emote.name).join(' ')) : 'None.'}` }
+                  rm.result = {
+                    value: `${rm.global === true ? 'global ' : ''}match's favorite emotes: ` +
+                    `${profile.favoriteEmotes.length > 0 ? profile.favoriteEmotes.map(emote => emote.name).join(' ') : 'None.'}`,
+                  }
                   socket.send(this.socketMessage(MessageType.ROLLMATCH, JSON.stringify(rm)))
                   break
                 case More.FAVORITESTREAMERS: {
                   await user.preload('favoriteStreamers')
 
-                  rm.result = { value: `${rm.global === true ? 'global ' : ''}match's favorite streamers: ${user.favoriteStreamers.length > 0 ? String(user.favoriteStreamers.map(streamer => `${streamer.name.substr(0, 1)}\u{E0000}${streamer.name.substr(1)}`).join(', ')) : 'None'}.` }
+                  const favoriteStreamers = user.favoriteStreamers.map(streamer => `${streamer.name.substr(0, 1)}\u{E0000}${streamer.name.substr(1)}`).join(', ')
+
+                  rm.result = {
+                    value: `${rm.global === true ? 'global ' : ''}match's favorite streamers: ` +
+                    `${user.favoriteStreamers.length > 0 ? favoriteStreamers : 'None'}.`,
+                  }
                   socket.send(this.socketMessage(MessageType.ROLLMATCH, JSON.stringify(rm)))
                   break
                 }
