1c32f8ad24a95fd1d3ee66bed46ad6f10ca6e301
Karar Al-Remahy <alremahykarar@ymail.com>
2020-08-16 03:42:41 +0200

    fix: Users need access to register URL.

--- start/routes/login.ts
+++ start/routes/login.ts
@@ -1,6 +1,6 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/register', 'AuthController.register').middleware('auth:web')
+Route.get('/register', 'AuthController.register')
 
 Route.post('/logout', 'AuthController.logout').middleware('auth:web')
 
