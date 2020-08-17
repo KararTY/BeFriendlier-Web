66084150e6b7581d8d257efceefce974f21904c5
Karar Al-Remahy <alremahykarar@ymail.com>
2020-08-17 18:33:08 +0200

    fix: Another attempt to fix error pages.

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -19,8 +19,8 @@ import Logger from '@ioc:Adonis/Core/Logger'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   protected statusPages = {
-    '404': 'errors/notFound',
-    '500..599': 'errors/serverError',
+    '404': 'errors.not-found',
+    '500..599': 'errors.server-error',
   }
 
   constructor () {
