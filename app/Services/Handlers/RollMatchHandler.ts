867c8129cb8a28937222de8d787153f810cdf396
Karar Al-Remahy <alremahykarar@ymail.com>
2021-03-20 19:07:12 +0100

    fix: Linting RollMatchHandler

--- app/Services/Handlers/RollMatchHandler.ts
+++ app/Services/Handlers/RollMatchHandler.ts
@@ -6,46 +6,46 @@ import Handler from '../Handler'
 export default class RollMatchHandler extends DefaultHandler {
   public messageType = MessageType.ROLLMATCH
 
-  public async onClientResponse (socket: ExtendedWebSocket, res: ResSchema) {
-    if (res.data !== undefined) {
-      const rm: ROLLMATCH = JSON.parse(res.data)
-      const { user, profile } = await Handler.rollMatch(rm)
+  public async onClientResponse(socket: ExtendedWebSocket, res: ResSchema): Promise<void> {
+    if (res.data === undefined) return
 
-      const bio = profile.bio.split(' ').map(word => `${word.substr(0, 1)}\u{E0000}${word.substr(1)}`).join(' ')
+    const rm: ROLLMATCH = JSON.parse(res.data)
+    const { user, profile } = await Handler.rollMatch(rm)
 
-      switch (rm.more) {
-        case More.NONE:
-          rm.result = {
-            value: `new ${rm.global === true ? 'global ' : ''}match's bio: ` +
+    const bio = profile.bio.split(' ').map(word => `${word.substr(0, 1)}\u{E0000}${word.substr(1)}`).join(' ')
+
+    switch (rm.more) {
+      case More.NONE:
+        rm.result = {
+          value: `new ${rm.global === true ? 'global ' : ''}match's bio: ` +
             `${profile.bio.length > 32 ? `${bio.substr(0, 32)}...` : bio}, reply with %prefix%more, %prefix%match or %prefix%no`,
-          }
-          socket.send(this.ws.socketMessage(MessageType.ROLLMATCH, JSON.stringify(rm)))
-          break
-        case More.BIO:
-          rm.result = {
-            value: `${rm.global === true ? 'global\'s ' : ''}full bio: ` +
-            `${bio}`,
-          }
-          socket.send(this.ws.socketMessage(MessageType.ROLLMATCH, JSON.stringify(rm)))
-          break
-        case More.FAVORITEEMOTES:
-          rm.result = {
-            value: `${rm.global === true ? 'global ' : ''}match's favorite emotes: ` +
-            `${profile.favoriteEmotes.length > 0 ? profile.favoriteEmotes.map(emote => emote.name).join(' ') : 'None.'}`,
-          }
-          socket.send(this.ws.socketMessage(MessageType.ROLLMATCH, JSON.stringify(rm)))
-          break
-        case More.FAVORITESTREAMERS: {
-          await user.preload('favoriteStreamers')
-
-          const favoriteStreamers = user.favoriteStreamers.map(streamer => `${streamer.name.substr(0, 1)}\u{E0000}${streamer.name.substr(1)}`).join(', ')
-
-          rm.result = {
-            value: `${rm.global === true ? 'global ' : ''}match's favorite streamers: ` +
-            `${user.favoriteStreamers.length > 0 ? favoriteStreamers : 'None'}.`,
-          }
-          socket.send(this.ws.socketMessage(MessageType.ROLLMATCH, JSON.stringify(rm)))
         }
+        socket.send(this.ws.socketMessage(MessageType.ROLLMATCH, JSON.stringify(rm)))
+        break
+      case More.BIO:
+        rm.result = {
+          value: `${rm.global === true ? 'global\'s ' : ''}full bio: ` +
+            `${bio}`,
+        }
+        socket.send(this.ws.socketMessage(MessageType.ROLLMATCH, JSON.stringify(rm)))
+        break
+      case More.FAVORITEEMOTES:
+        rm.result = {
+          value: `${rm.global === true ? 'global ' : ''}match's favorite emotes: ` +
+            `${profile.favoriteEmotes.length > 0 ? profile.favoriteEmotes.map(emote => emote.name).join(' ') : 'None.'}`,
+        }
+        socket.send(this.ws.socketMessage(MessageType.ROLLMATCH, JSON.stringify(rm)))
+        break
+      case More.FAVORITESTREAMERS: {
+        await user.preload('favoriteStreamers')
+
+        const favoriteStreamers = user.favoriteStreamers.map(streamer => `${streamer.name.substr(0, 1)}\u{E0000}${streamer.name.substr(1)}`).join(', ')
+
+        rm.result = {
+          value: `${rm.global === true ? 'global ' : ''}match's favorite streamers: ` +
+            `${user.favoriteStreamers.length > 0 ? favoriteStreamers : 'None'}.`,
+        }
+        socket.send(this.ws.socketMessage(MessageType.ROLLMATCH, JSON.stringify(rm)))
       }
     }
   }
