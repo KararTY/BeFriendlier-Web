421febd90efe77186dc7290b694136f23d6178da
Karar Al-Remahy <alremahykarar@ymail.com>
2021-08-17 11:06:16 +0200

    fix: Don't always return emote count when trading.

--- app/Services/Handlers/GiveEmotesHandler.ts
+++ app/Services/Handlers/GiveEmotesHandler.ts
@@ -15,17 +15,10 @@ export default class GiveEmotesHandler extends DefaultHandler {
     data.emotes = sentEmotes
 
     data.result = {
-      value: `you gave: ${sentEmotes.map(e => `x${e.amount} ${e.name}`).join(' ')}` +
+      value: `you gave: ${sentEmotes.map(e => `${(e.amount || 1) > 1 ? `x${e.amount} ` : ''}${e.name}`).join(' ')}` +
         ` to ${data.recipientUserTwitch.name}!`,
     }
 
     socket.send(this.ws.socketMessage(MessageType.GIVEEMOTES, JSON.stringify(data)))
-
-    // Attempted to match. Must wait for receiving end.
-    // data.result = {
-    //   value: `you've sent ${sentEmotes.map(res => res.amount ?? 0).flat(1).join('')} emotes to user ${}.`,
-    // }
-
-    // socket.send(this.ws.socketMessage(MessageType.MATCH, JSON.stringify(data)))
   }
 }
