82538308ad82b019cfcd0841f78bd944ee9b79c4
Karar Al-Remahy <alremahykarar@ymail.com>
2022-10-06 19:06:39 +0200

    fix: Linting issues.

--- resources/js/BattleLogs.tsx
+++ resources/js/BattleLogs.tsx
@@ -1,4 +1,4 @@
-import { createMemo, createResource, createSignal, For, Show } from 'solid-js'
+import { createMemo, createResource, createSignal, For, JSXElement, Show } from 'solid-js'
 import { render } from 'solid-js/web'
 
 import { getStatColor } from '../../providers/Battle'
@@ -6,20 +6,30 @@ import { paginate } from './PaginationHelper'
 
 const [page, setPage] = createSignal()
 
-const fetchLogs = async (page = 0) => (await fetch(`/emotes/logs?page=${page}`)).json()
+const fetchLogs = async (page = 0): Promise<any> => await (await fetch(`/emotes/logs?page=${page}`)).json()
+
 const [logsResponse, { refetch }] = createResource(page, fetchLogs)
 
-function Statistic ({ statistic }) {
+function Statistic ({ statistic }): JSXElement {
   return (
     <p>
       <strong>{statistic.name}</strong>
-      {typeof statistic.defValue === 'number' && typeof statistic.curValue === 'number' &&
-        <><strong>: </strong><small>{statistic.curValue.toFixed(2)}{statistic.defValue !== statistic.curValue ? '/' + statistic.defValue.toFixed(2) : undefined}</small></>}
+      {typeof statistic.defValue === 'number' && typeof statistic.curValue === 'number' && (
+        <>
+          <strong>: </strong>
+          <small>
+            {statistic.curValue.toFixed(2)}
+            {statistic.defValue !== statistic.curValue
+              ? `/${statistic.defValue.toFixed(2)}`
+              : undefined}
+          </small>
+        </>
+      )}
     </p>
   )
 }
 
-function BattleLog({ date, won, statistics, participants, images }) {
+function BattleLog ({ date, won, statistics, participants, images }): JSXElement {
   const [fullStatus, setFullStatus] = createSignal(false)
 
   return (
@@ -29,8 +39,12 @@ function BattleLog({ date, won, statistics, participants, images }) {
           <div class='card-header-title' title={new Date(date).toISOString()}>
             {new Date(date).toLocaleString()}
           </div>
-          <button class={`card-header-icon is-family-monospace ${won ? 'has-text-success' : 'has-text-danger'}`} onClick={() => setFullStatus(!fullStatus())}>
-            {won ? (fullStatus() ? 'WIN' : 'W') : (fullStatus() ? 'LOSS' : 'L')}
+          <button
+            class={`card-header-icon is-family-monospace ${won ? 'has-text-success' : 'has-text-danger'
+              }`}
+            onClick={() => setFullStatus(!fullStatus())}
+          >
+            {won ? (fullStatus() ? 'WIN' : 'W') : fullStatus() ? 'LOSS' : 'L'}
           </button>
         </div>
         <div class='card-content'>
@@ -38,11 +52,27 @@ function BattleLog({ date, won, statistics, participants, images }) {
             <div class='level-left'>
               <div class='level-item'>
                 <figure class='image is-32x32'>
-                  <img class='is-rounded' src={'/img/battle_emotes/'+ images[0].localUrl} alt={images[0].name + ' emote image.'} title={`${images[0].name} drawn by @${images[0].author}.`} />
+                  <img
+                    class='is-rounded'
+                    src={'/img/battle_emotes/' + images[0].localUrl}
+                    alt={images[0].name + ' emote image.'}
+                    title={`${images[0].name} drawn by @${images[0].author}.`}
+                  />
                 </figure>
               </div>
               <div class='level-item'>
-                <span>{images[0].name} {statistics[0][4] && <span class={'tag p-1 has-background-dark has-text-' + getStatColor(statistics[0][4].name).split('-').pop()}>{`[${statistics[0][4].name}]`}</span>}</span>
+                <span>
+                  {images[0].name}{' '}
+                  {statistics[0][4] && (
+                    <span
+                      class={
+                        'tag p-1 has-background-dark has-text-' +
+                        getStatColor(statistics[0][4].name).split('-').pop()
+                      }
+                    >{`[${statistics[0][4].name}]`}
+                    </span>
+                  )}
+                </span>
               </div>
               <div class='level-item'>
                 <span>{participants[0]}</span>
@@ -53,11 +83,27 @@ function BattleLog({ date, won, statistics, participants, images }) {
                 <span>{participants[1]}</span>
               </div>
               <div class='level-item'>
-                <span>{images[1].name} {statistics[1][4] && <span class={'tag p-1 has-background-dark has-text-' + getStatColor(statistics[1][4].name).split('-').pop()}>{`[${statistics[1][4].name}]`}</span>}</span>
+                <span>
+                  {images[1].name}{' '}
+                  {statistics[1][4] && (
+                    <span
+                      class={
+                        'tag p-1 has-background-dark has-text-' +
+                        getStatColor(statistics[1][4].name).split('-').pop()
+                      }
+                    >{`[${statistics[1][4].name}]`}
+                    </span>
+                  )}
+                </span>
               </div>
               <div class='level-item'>
                 <figure class='image is-32x32'>
-                  <img class='is-rounded' src={'/img/battle_emotes/'+ images[1].localUrl} alt={images[1].name + ' emote image.'} title={`${images[1].name} drawn by @${images[1].author}.`} />
+                  <img
+                    class='is-rounded'
+                    src={'/img/battle_emotes/' + images[1].localUrl}
+                    alt={images[1].name + ' emote image.'}
+                    title={`${images[1].name} drawn by @${images[1].author}.`}
+                  />
                 </figure>
               </div>
             </div>
@@ -72,7 +118,9 @@ function BattleLog({ date, won, statistics, participants, images }) {
                         <strong>Level: </strong>
                         <span> {statistics[0][0].defValue}</span>
                       </p>
-                      <For each={statistics[0].slice(1)}>{(statistic: any) => <Statistic statistic={statistic} />}</For>
+                      <For each={statistics[0].slice(1)}>
+                        {(statistic: any) => <Statistic statistic={statistic} />}
+                      </For>
                     </div>
                   </div>
                   <div class='level-right'>
@@ -81,13 +129,20 @@ function BattleLog({ date, won, statistics, participants, images }) {
                         <strong>Level: </strong>
                         <span> {statistics[1][0].defValue}</span>
                       </p>
-                      <For each={statistics[1].slice(1)}>{(statistic: any) => <Statistic statistic={statistic} />}</For>
+                      <For each={statistics[1].slice(1)}>
+                        {(statistic: any) => <Statistic statistic={statistic} />}
+                      </For>
                     </div>
                   </div>
                 </div>
               </div>
             </Show>
-            <button class='button is-fullwidth is-small' onClick={() => setFullStatus(!fullStatus())}>{fullStatus() ? 'Hide' : 'Show'} statistics</button>
+            <button
+              class='button is-fullwidth is-small'
+              onClick={() => setFullStatus(!fullStatus())}
+            >
+              {fullStatus() ? 'Hide' : 'Show'} statistics
+            </button>
           </div>
         </div>
       </div>
@@ -95,33 +150,58 @@ function BattleLog({ date, won, statistics, participants, images }) {
   )
 }
 
-function Pagination({ maxPage }) {
-  const pagination  = createMemo(() => paginate({ current: page() as number, maxPage }))
+function Pagination ({ maxPage }) {
+  const pagination = createMemo(() => paginate({ current: page() as number, maxPage }))
 
   return (
     <nav class='pagination is-centered' role='navigation' aria-label='pagination'>
-      <button class={`button pagination-previous ${pagination()?.prev && 'is-disabled'}`} disabled={!pagination()?.prev} onClick={() => pagination()?.prev && setPage((page() as number) - 1)}>Previous</button>
-      <button class={`button pagination-next ${pagination()?.next && 'is-disabled'}`} disabled={!pagination()?.next} onClick={() => pagination()?.next && setPage((page() as number) + 1)}>Next page</button>
+      <button
+        class={`button pagination-previous ${pagination()?.prev && 'is-disabled'}`}
+        disabled={!pagination()?.prev}
+        onClick={() => pagination()?.prev && setPage((page() as number) - 1)}
+      >
+        Previous
+      </button>
+      <button
+        class={`button pagination-next ${pagination()?.next && 'is-disabled'}`}
+        disabled={!pagination()?.next}
+        onClick={() => pagination()?.next && setPage((page() as number) + 1)}
+      >
+        Next page
+      </button>
       <ul class='pagination-list'>
-        <For each={pagination()?.items}>{(item) =>
-          <li>
-            {typeof item === 'number' && <button class={`button pagination-link ${page() === item && 'is-current'}`} aria-label={'Goto page ' + item} aria-current={page() === item ? 'page' : undefined} onClick={() => setPage(item)}>{item}</button>}
-            {typeof item === 'string' && <span class='pagination-ellipsis'>&hellip;</span>}
-          </li>
-        }</For>
+        <For each={pagination()?.items}>
+          {(item) => (
+            <li>
+              {typeof item === 'number' && (
+                <button
+                  class={`button pagination-link ${page() === item && 'is-current'}`}
+                  aria-label={`Goto page ${item}`}
+                  aria-current={page() === item ? 'page' : undefined}
+                  onClick={() => setPage(item)}
+                >
+                  {item}
+                </button>
+              )}
+              {typeof item === 'string' && <span class='pagination-ellipsis'>&hellip;</span>}
+            </li>
+          )}
+        </For>
       </ul>
     </nav>
   )
 }
 
-function BattleLogsApp() {
+function BattleLogsApp () {
   return (
     <div class='columns is-multiline'>
       <div class='column is-12'>
         {logsResponse()?.maxPage && <Pagination maxPage={logsResponse().maxPage} />}
       </div>
 
-      {logsResponse()?.logs && <For each={logsResponse().logs}>{(log: any) => <BattleLog {...log}/>}</For>}
+      {logsResponse()?.logs && (
+        <For each={logsResponse().logs}>{(log: any) => <BattleLog {...log} />}</For>
+      )}
 
       <div class='column is-12'>
         {logsResponse()?.maxPage && <Pagination maxPage={logsResponse().maxPage} />}
@@ -130,28 +210,26 @@ function BattleLogsApp() {
   )
 }
 
-
 const el = document.getElementById('battleLogs')
 
-if (el) {
+if (el != null) {
   render(BattleLogsApp, el)
 }
 
 const battleTabsToggleBtn = document.querySelectorAll('[data-battle-tabs-toggle]')
 
-if (battleTabsToggleBtn) {
-  battleTabsToggleBtn.forEach(el =>
-    el.addEventListener('click', () => {
-      document.querySelector('[data-battle-logs]')?.classList.toggle('is-hidden')
-      document.querySelector('[data-battle-emotes]')?.classList.toggle('is-hidden')
+battleTabsToggleBtn.forEach((el) =>
+  el.addEventListener('click', () => {
+    document.querySelector('[data-battle-logs]')?.classList.toggle('is-hidden')
+    document.querySelector('[data-battle-emotes]')?.classList.toggle('is-hidden')
 
-      // We only want to run the fetch when user actually clicks on Battle Logs button.
-      if (typeof page() !== 'number') setPage(1)
-  }))
-}
+    // We only want to run the fetch when user actually clicks on Battle Logs button.
+    if (typeof page() !== 'number') setPage(1)
+  })
+)
 
 const battleLogsRefreshBtn = document.querySelector('[data-battle-logs-reload]')
 
-if (battleLogsRefreshBtn) {
+if (battleLogsRefreshBtn != null) {
   battleLogsRefreshBtn.addEventListener('click', () => refetch())
 }
--- resources/js/BattlesLeaderboard.tsx
+++ resources/js/BattlesLeaderboard.tsx
@@ -1,29 +1,41 @@
-import { createResource, createSignal, For } from 'solid-js'
+import { createResource, createSignal, For, JSXElement } from 'solid-js'
 import { render } from 'solid-js/web'
 
 const [time, setTime] = createSignal(new Date().getTime())
 
-const fetchLogs = async () => (await fetch(`/leaderboards/battles`)).json()
+const fetchLogs = async (): Promise<any> => await (await fetch('/leaderboards/battles')).json()
 const [leaderboardResponse] = createResource(time, fetchLogs)
 
 // Refresh battle leaderboard every hour-ish.
 setInterval(() => {
   setTime(new Date().getTime())
-}, ((1000 * 60) * 60) + (Math.random() * 1000))
+}, 1000 * 60 * 60 + Math.random() * 1000)
 
-function BattleLeaderboardEntry({ index, avatar, display_name, name, total_wins, total_losses }) {
+function BattleLeaderboardEntry ({ index, avatar = '', display_name = '', name = '', total_wins = '', total_losses = '' }): JSXElement {
   return (
     <tr>
-      <th>{index + 1}</th>
+      <th>{Number(index) + 1}</th>
       <td>
-        <div class="level is-mobile">
-          <div class="level-left">
-            <figure class="level-item image is-32x32 is-clipped">
-              <img class={`is-rounded${globalThis.streamerMode ? ' blur' : ''}`} data-blur-vanish={globalThis.streamerMode ? 'true' : undefined} data-blur={globalThis.streamerMode ? 'channel-' + name : undefined} src={avatar.replace('300x300', '70x70')} alt={`${display_name}'s avatar`} />
+        <div class='level is-mobile'>
+          <div class='level-left'>
+            <figure class='level-item image is-32x32 is-clipped'>
+              <img
+                class={`is-rounded${globalThis.streamerMode ? ' blur' : ''}`}
+                data-blur-vanish={globalThis.streamerMode ? 'true' : undefined}
+                data-blur={globalThis.streamerMode ? 'channel-' + name : undefined}
+                src={avatar.replace('300x300', '70x70')}
+                alt={`${display_name}'s avatar`}
+              />
             </figure>
-            <span class="level-item">{display_name}{name !== display_name.toLowerCase() ? ' (' + name + ')' : ''}</span>
-            {index === 0 &&
-              <span class="level-item" title="The Queen Warrior duck!">(👑🥦🦆)</span>}
+            <span class='level-item'>
+              {display_name}
+              {name !== display_name.toLowerCase() ? ` (${name})` : ''}
+            </span>
+            {index === 0 && (
+              <span class='level-item' title='The Queen Warrior duck!'>
+                (👑🥦🦆)
+              </span>
+            )}
           </div>
         </div>
       </td>
@@ -37,35 +49,50 @@ function BattleLeaderboardEntry({ index, avatar, display_name, name, total_wins,
   )
 }
 
-function BattlesLeaderboardApp() {
+function BattlesLeaderboardApp (): JSXElement {
   return (
-    <table class={`table is-fullwidth animated${leaderboardResponse.loading ? ' fadeOut' : 'fadeIn'}`}>
+    <table
+      class={`table is-fullwidth animated${leaderboardResponse.loading ? ' fadeOut' : 'fadeIn'}`}
+    >
       <thead>
         <tr>
-          <th><abbr title="Position">Pos</abbr></th>
+          <th>
+            <abbr title='Position'>Pos</abbr>
+          </th>
           <th>User</th>
-          <th><abbr title="Total Wins">W</abbr></th>
-          <th><abbr title="Total Losses">L</abbr></th>
+          <th>
+            <abbr title='Total Wins'>W</abbr>
+          </th>
+          <th>
+            <abbr title='Total Losses'>L</abbr>
+          </th>
         </tr>
       </thead>
       <tfoot>
         <tr>
-          <th><abbr title="Position">Pos</abbr></th>
+          <th>
+            <abbr title='Position'>Pos</abbr>
+          </th>
           <th>User</th>
-          <th><abbr title="Total Wins">W</abbr></th>
-          <th><abbr title="Total Losses">L</abbr></th>
+          <th>
+            <abbr title='Total Wins'>W</abbr>
+          </th>
+          <th>
+            <abbr title='Total Losses'>L</abbr>
+          </th>
         </tr>
       </tfoot>
       <tbody>
-        <For each={leaderboardResponse()}>{(entry: any, index) => <BattleLeaderboardEntry index={index()} {...entry()} />}</For>
+        <For each={leaderboardResponse()}>
+          {(entry: any, index) => <BattleLeaderboardEntry index={index()} {...entry()} />}
+        </For>
       </tbody>
     </table>
   )
 }
 
-
 const el = document.getElementById('battlesLeaderboard')
 
-if (el) {
+if (el !== null) {
   render(BattlesLeaderboardApp, el)
 }
